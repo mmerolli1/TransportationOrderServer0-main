83bf1074ff3f61cd27415d200692837e2d1b39af
Martina Merolli <martinamerolli@AirdiMartina2.home>
2023-11-29 20:04:24 +0100

    repository in github

---
+++ src/main/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplication.java
@@ -0,0 +1,32 @@
+package es.upm.dit.apsv.transportationorderserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*@SpringBootApplication
+public class TransportationOrderServerApplication{
+  
+  public static void main(String[] args) {
+		SpringApplication.run(TransportationOrderServerApplication.class, args);
+  }
+
+}*/
+
+@SpringBootApplication
+public class TransportationOrderServerApplication {
+
+	public static final Logger log = LoggerFactory.getLogger(TransportationOrderServerApplication.class);
+
+	private TransportationOrderRepository torderRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationOrderServerApplication.class, args);
+	}
+
+}
